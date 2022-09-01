7f203e90059c75303be028698cda0525f14ff157
BaronBecker <60110113+BaronBecker@users.noreply.github.com>
2022-08-31 23:03:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class machines(db.Model):
     employee = db.Column(db.String(200), nullable = False)
     machine = db.Column(db.String(200), nullable = False)
     task = db.Column(db.String(200), nullable = False)
-    hours = db.Column(db.Integer, nullable = False)
+    hours = db.Column(db.Integer)
     note = db.Column(db.String(200), nullable = False)
 
     def __repr__(self):
